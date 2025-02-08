a120580da991237b110313990ee95d13eac74912
mostafahala <94720969+mostafa-hala@users.noreply.github.com>
2025-02-08 16:05:17 +0200

    get_random_response

---
+++ code.py
@@ -0,0 +1,15 @@
+import random
+
+responses = [
+    "Yes, definitely!",
+    "No, not now.",
+    "Ask again later.",
+    "It is certain.",
+    "Very doubtful.",
+    "Outlook is good.",
+    "Better not tell you now.",
+    "Concentrate and ask again."
+]
+
+def get_random_response():
+    return random.choice(responses)
